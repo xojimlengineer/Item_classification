df8cbec91ee561d087ac86700cac5ae899c26657
Xojiakbar <jumaboyevhojiakbar7@gmail.com>
2023-08-15 22:43:44 +0500

    project files have been iploaded

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from fastai.vision.all import*
+import plotly.express as px
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+# title
+st.title("Transportni klassifikatsiya qiluvchi model")
+
+# rasm yuklash
+file=st.file_uploader("rasm yuklash",type=['png','jpeg','gif','svg','jfif'])
+if file:
+    st.image(file)
+
+    #Pl convert
+    img=PILImage.create(file)
+    
+    #model
+    model=load_learner("transport_model.pkl")
+    
+    #prediction
+    predic,index_id,pro=model.predict(img)
+    st.success(f"Bashorat : {predic}")
+    st.info(f"Ehtimollik:{pro[index_id]*100:.1f}%")
+
+    # plotting
+    fig=px.bar(x=pro*100,y=model.dls.vocab)
+    st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+
+
+
+
