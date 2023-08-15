6ac711a416d23c2b2434674edb2dd588b3c55257
jumaboyev xojiakbar <109433027+xojimlengineer@users.noreply.github.com>
2023-08-15 23:47:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import pathlib
 import platform
 plt=platform.system()
-if plt=="Linux":pathlib.WindowsPath=pathlib.PosixPath
+if plt=='Linux':pathlib.WindowsPath=pathlib.PosixPath
 
 # title
 st.title("Transportni klassifikatsiya qiluvchi model")
